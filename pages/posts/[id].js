3aa4ad31063a39ab0bc99fb03ae74413baf8ef85
Olatunji Abayomi <abayomijohn273@gmail.com>
2021-08-25 16:03:53 +0100

    update

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,7 @@ import Image from "next/image"
 import Head from "next/head"
 import Layout from "../../components/layout"
 import Alert from "../../components/alert/alert"
+import { getAllPostIds } from "../../lib/posts"
 
 const Post = ({ post }) => {
     const router = useRouter();
@@ -31,21 +32,10 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-
+    const paths = getAllPostIds()
 
     return {
-        paths: [
-            {
-                params: {
-                    id: "1",
-                }
-            },
-            {
-                params: {
-                    id: "2",
-                }
-            }
-        ],
+        paths,
         fallback: false
     }
 }
